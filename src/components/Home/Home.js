d68221b57f1b61bab9a52b74c17b45177326de3c
m-m-e <megmyfedwards@gmail.com>
2020-01-18 22:41:13 +0100

    Add grouping functionality.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { data } from '../../data';
+import './home.css';
 
-const Home = () =>
-<section>
-  <h1>Home</h1>
-</section>
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      group: [],
+    };
+    this.handleCardClick = this.handleCardClick.bind(this);
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
+  }
+
+  handleCardClick(event) {
+    const { id } = event.currentTarget;
+    const chosenCard = data.find(element => id === element.id.toString());
+    this.setState({ group: [...this.state.group, chosenCard]})
+  }
+
+  handleRemoveClick(event) {
+    const { id } = event.currentTarget.parentElement;
+    const { group } = this.state;
+    const newGroup = group.filter(element => id !== element.id.toString());
+    this.setState({ group: newGroup })
+  }
+
+  render() {
+    const { group } = this.state;
+    const { handleCardClick, handleRemoveClick } = this;
+    return (
+      <div className='home'>
+        <section className='search_section'>
+          <h1 className='home_title'>Home</h1>
+
+          <div className='search'>
+            <label htmlFor='search'></label>
+            <input type='text' name='search'></input>
+            <button className='search_button'>Search</button>
+          </div>
+
+          <div className='search_cards'>
+          {data.map(({ name, id, hp }) => 
+            <div 
+              className='card search_card'
+              key={id}
+              onClick={handleCardClick}
+              id={id}
+            >
+              <p>Name: {name}</p>
+              <p>HP: {hp}</p>
+            </div>
+          )}
+          </div>
+        </section>
+        <section>
+          <h2 className='group_section'>Group</h2>
+          <div className='group_cards'>
+          {group.map(({ name, id, hp }) =>
+            <div 
+              className='card_container'
+              key={id}
+              id={id}
+            >
+              <div className='card group_card'>
+                <p>{name}</p>
+                <p>{hp}</p>
+              </div>
+            <p className='remove-button' onClick={handleRemoveClick}>X</p>
+            </div>
+          )}
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
 
 export default Home;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 0,
+    name: 'Ed',
+    hp: 50,
+  },
+  {
+    id: 1,
+    name: 'AJ',
+    hp: 50,
+  },
+  {
+    id: 2,
+    name: 'Paul',
+    hp: 50,
+  },
+];
